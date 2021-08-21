17796fecde96beb1de6488b3e87d24ee508b7c68
Duane Shelton <dshelt10@uncc.edu>
2021-08-21 00:23:43 -0400

    flsk app #4 - added db connection toa pp

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -7,8 +7,16 @@ from flask import Flask
 from flask import render_template
 from flask import request  # Flask is the web app that we will customize
 from flask import redirect, url_for
+from database import db
 
 app = Flask(__name__)     # create an app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
 notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
          2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
          3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
