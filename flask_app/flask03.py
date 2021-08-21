24b8bdc78d7de51882ee1c935759d241864fe090
Duane Shelton <dshelt10@uncc.edu>
2021-08-20 20:45:53 -0400

    flask app #3 - capturing data in POST request

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -39,7 +39,8 @@ def new_note():
     # check method used for request
     print('request method is', request.method)
     if request.method == 'POST':
-        return '<h1> POST method used for this request </h1>'
+        request_data = request.form
+        return f"data: {request_data} !"
     else:
             return render_template('new.html', user=a_user)
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
