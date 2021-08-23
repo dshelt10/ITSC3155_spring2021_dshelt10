4f8edd52639c24f41db41e1030a61b842d7231fd
Duane Shelton <dshelt10@uncc.edu>
2021-08-23 04:09:37 -0400

    adding and displaying comments functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -14,6 +14,10 @@ from forms import RegisterForm
 from flask import session
 import bcrypt
 from forms import LoginForm
+from models  import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
+
+
 app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
@@ -50,11 +54,15 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='dshelt10@uncc.edu').one()
+    if session.get('user'):
 
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
+        my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
 
-    return render_template('note.html', note = my_note, user=a_user)
+        form = CommentForm()
+
+        return render_template('note.html', note=my_note, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
 
 
 
@@ -177,6 +185,24 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 app.config['SECRET_KEY'] = 'SE3155'
 
