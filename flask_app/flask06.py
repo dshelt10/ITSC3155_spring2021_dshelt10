e3e37f6b692377f56e4781e0c06b9bef8545f27b
Duane Shelton <dshelt10@uncc.edu>
2021-08-23 03:57:53 -0400

    added logout functionality and updated user navigation links

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -169,6 +169,14 @@ def login():
     else:
         # form did not validate or GET request
         return render_template("login.html", form=login_form)
+    
+@app.route('/logout')
+def logout():
+    # check if a user is saved in session
+    if session.get('user'):
+        session.clear()
+
+    return redirect(url_for('index'))
 
 app.config['SECRET_KEY'] = 'SE3155'
 
